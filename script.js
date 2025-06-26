92f7e4b32f54e8017b294aa29dab9ef840bb92e1
MirMuhammadNajam <mir.najam.005899@headstart.edu.pk>
2025-06-26 11:18:04 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function showGreeting() {
-    const greeting = document.getElementById('greeting');
-    greeting.textContent = 'Bye.';
-  }
-  
+function closeWebsite() {
+    
+    window.open('', '_self', '');
+    window.close();
+
+}
+function openMultipleTabs() {
+    for (let i = 0; i < 3; i++) {
+        window.open(window.location.href, '_blank');
+    }
+}
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("open");
+}
